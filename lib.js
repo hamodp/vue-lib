7de62d700f166e155eae0be91fbb8157917b7596
Patrick Hamod <pfhamod@gmail.com>
2020-02-19 17:17:40 -0800

    added ability to publish locally and build as lib

---
+++ lib.js
@@ -0,0 +1,4 @@
+import DummyButton from './components/DummyButton.vue'
+console.log("patrick importing vuetify")
+
+export default DummyButton
---
+++ src/components/DummyButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <v-card>
+        <v-btn @click="increment">{{ text }}</v-btn>
+      </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  props: {
+    attr: Number
+  },
+  computed: {
+    times () {
+      return this.count > 1
+        ? 'times'
+        : 'time'
+    },
+    text () {
+      return `I have been clicked ${this.count} ${this.times}`
+    }
+  },
+  methods: {
+    increment () {
+      this.count += 1
+      this.$emit('count', this.count)
+      this.$parent.$emit('count', this.count)
+      console.log(this.$parent)
+    },
+    test (){
+      console.log('testing emit')
+    }
+  },
+  mounted() {
+    this.$on('count', this.test)
+  }
+}
+</script>
